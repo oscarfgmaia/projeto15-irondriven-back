2d42df0c96ec997b2cba778d213b81a5d58b7c27
Oscar Maia <oscarfgmaia@gmail.com>
2022-11-28 02:44:11 -0300

    getProduct route

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -1,3 +1,4 @@
+import { ObjectId } from 'mongodb';
 import { productsCollection } from '../database/database.js'
 
 export async function createProducts(req, res) {
@@ -23,6 +24,20 @@ export async function getProducts(req, res) {
             return res.sendStatus(404);
         }
         res.status(200).send(products);
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
+}
+
+export async function getProduct(req, res) {
+    const id = req.params.id
+    try {
+        const product = await productsCollection.findOne({ _id: ObjectId(id) })
+        if (!product) {
+            return res.sendStatus(404);
+        }
+        res.status(200).send(product);
     } catch (error) {
         console.log(error);
         res.sendStatus(500);
