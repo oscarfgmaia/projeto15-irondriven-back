dae0683f67d554ee844b2fb5d0a51bab30b7ead3
Oscar Maia <oscarfgmaia@gmail.com>
2022-11-28 00:25:27 -0300

    create get products route

---
+++ src/controllers/product.controller.js
@@ -0,0 +1,30 @@
+import { productsCollection } from '../database/database.js'
+
+export async function createProducts(req, res) {
+    const product = res.locals.product
+    const newProduct = {
+        ...product, sold_amount: 0
+    }
+    try {
+        await productsCollection.insertOne(newProduct);
+        res.sendStatus(201);
+    } catch (err) {
+        console.log(err);
+        res.sendStatus(500);
+    }
+
+}
+
+
+export async function getProducts(req, res) {
+    try {
+        const products = await productsCollection.find({}).toArray();
+        if (!products || products.length == 0) {
+            return res.sendStatus(404);
+        }
+        res.status(200).send(products);
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
+}
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,11 +1,12 @@
 import getUsersRouter from "./getUsersRouter.js";
 import productsrouter from './productsRouter.js'
-
+import getProductsRouter from "./getProductsRouter.js"
 
 import { Router } from "express";
 const router = Router();
 
 router.use(getUsersRouter);
 router.use(productsrouter);
+router.use(getProductsRouter)
 
 export default router;
